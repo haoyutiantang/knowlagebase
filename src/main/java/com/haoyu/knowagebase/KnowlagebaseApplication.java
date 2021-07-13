8179fcf56f82d90f3b7035dc8fdfe427e05d08be
haoyu <1457773856@qq.com>
2021-07-14 07:27:50 +0800

    构建初始项目

---
+++ src/main/java/com/haoyu/knowagebase/KnowlagebaseApplication.java
@@ -0,0 +1,16 @@
+package com.haoyu.knowagebase;
+/*
+ * @author haoyu
+ * @date 2021/7/14 7:23
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowlagebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowlagebaseApplication.class, args);
+    }
+
+}
