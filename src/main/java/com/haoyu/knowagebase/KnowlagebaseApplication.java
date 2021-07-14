8067f19c627830f6285087b217f87b2dd8889822
haoyu <1457773856@qq.com>
2021-07-14 23:20:36 +0800

    修改启动文案

--- src/main/java/com/haoyu/knowagebase/KnowlagebaseApplication.java
+++ src/main/java/com/haoyu/knowagebase/KnowlagebaseApplication.java
@@ -3,14 +3,22 @@ package com.haoyu.knowagebase;
  * @author haoyu
  * @date 2021/7/14 7:23
  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class KnowlagebaseApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(KnowlagebaseApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(KnowlagebaseApplication.class, args);
+        SpringApplication app = new SpringApplication(KnowlagebaseApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
