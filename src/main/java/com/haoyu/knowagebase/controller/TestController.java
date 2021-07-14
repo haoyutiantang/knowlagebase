75a146ada23fffbd5340084b5455173cac44d525
haoyu <1457773856@qq.com>
2021-07-14 23:57:35 +0800

    增加hello接口测试test.http

--- src/main/java/com/haoyu/knowagebase/controller/TestController.java
+++ src/main/java/com/haoyu/knowagebase/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.haoyu.knowagebase.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /*
  * @author haoyu
@@ -12,5 +15,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world ! Post" + name;
     }
 }
