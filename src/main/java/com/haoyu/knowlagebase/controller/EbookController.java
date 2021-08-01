c12c0501fe491f25ffc7a42d2b566ac5000f2457
haoyu <1457773856@qq.com>
2021-08-01 17:56:30 +0800

    增加了一个通用的返回类CommonResp,以后所有后端的接口都是CommonResp类型

--- src/main/java/com/haoyu/knowlagebase/controller/EbookController.java
+++ src/main/java/com/haoyu/knowlagebase/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.haoyu.knowlagebase.controller;
 
 import com.haoyu.knowlagebase.domain.Ebook;
+import com.haoyu.knowlagebase.resp.CommonResp;
 import com.haoyu.knowlagebase.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
