dc04b08757901b227fe3763f01667f653102387c
haoyu <1457773856@qq.com>
2021-08-05 23:30:00 +0800

    增加AOP 打印接口耗时，请求参数和返回参数 去掉过滤器和拦截器

--- src/main/java/com/haoyu/knowlagebase/config/SpringMvcConfig.java
+++ src/main/java/com/haoyu/knowlagebase/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.haoyu.knowlagebase.config;
-
-
-import com.haoyu.knowlagebase.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.haoyu.knowlagebase.config;
+//
+//
+//import com.haoyu.knowlagebase.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
