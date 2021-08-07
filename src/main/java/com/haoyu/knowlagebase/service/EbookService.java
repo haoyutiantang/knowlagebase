b5013f3685fc08dda0f810cdf2e318589af611bb
haoyu <1457773856@qq.com>
2021-08-08 00:02:53 +0800

    集成PageHelper插件 将mapper日志级别改为trace

--- src/main/java/com/haoyu/knowlagebase/service/EbookService.java
+++ src/main/java/com/haoyu/knowlagebase/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.haoyu.knowlagebase.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.haoyu.knowlagebase.domain.Ebook;
 import com.haoyu.knowlagebase.domain.EbookExample;
 import com.haoyu.knowlagebase.mapper.EbookMapper;
 import com.haoyu.knowlagebase.req.EbookReq;
 import com.haoyu.knowlagebase.resp.EbookResp;
 import com.haoyu.knowlagebase.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +25,8 @@ import java.util.List;
  */
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -31,8 +37,11 @@ public class EbookService {
             criteria.andNameLike("%"+req.getName()+"%");
         }
         //模糊匹配
-
+        PageHelper.startPage(1,3);//只对第一个遇到的sql起作用  1.页码 2.每页的条数
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数:{}",pageInfo.getTotal());
+        LOG.info("总页数:{}",pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for(Ebook ebook : ebookList){
 //            //EbookResp ebookResp = new EbookResp();
