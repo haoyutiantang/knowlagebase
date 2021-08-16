62c6801c1ba3b5a78c0b3b21e50f7d58088d1d55
haoyu <1457773856@qq.com>
2021-08-16 20:30:34 +0800

    电子书列表显示分类名称

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -33,6 +33,9 @@
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar" />
         </template>
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
         <template v-slot:action="{text, record}">
           <a-space size = "small">
             <a-button type = "primary" @click="edit(record)">
@@ -110,12 +113,8 @@
           dataIndex: 'name',
         },
         {
-          title: '分类一',
-          dataIndex: 'category1Id',
-        },
-        {
-          title: '分类二',
-          dataIndex: 'category2Id',
+          title: '分类',
+          slots: {customRender: 'category'},
         },
         {
           title: '文档数',
@@ -235,7 +234,7 @@
       };
 
       const level1 =  ref();
-
+      let categorys: any;
       /**
        * 查询所有分类
        **/
@@ -245,7 +244,7 @@
           loading.value = false;
           const data = response.data;
           if (data.success) {
-            const categorys = data.content;
+            categorys = data.content;
             console.log("原始数组：", categorys);
 
             level1.value = [];
@@ -256,6 +255,19 @@
           }
         });
       };
+
+      const getCategoryName = (cid: number) => {
+        // console.log(cid)
+        let result = "";
+        categorys.forEach((item: any) => {
+          if (item.id === cid) {
+            // return item.name; // 注意，这里直接return不起作用
+            result = item.name;
+          }
+        });
+        return result;
+      };
+
       onMounted(() => {
         handleQueryCategory();
         handleQuery({
@@ -272,6 +284,8 @@
         loading,
         handleTableChange,
         handleQuery,
+        getCategoryName,
+
         edit,
         add,
 
