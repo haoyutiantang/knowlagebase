67ef3cb7cc1475bb58e4f3db950c7875747f509f
haoyu <1457773856@qq.com>
2021-08-17 22:58:47 +0800

    从分类管理复制出文档管理的前后端代码 遗留问题：编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/com/haoyu/knowlagebase/controller/DocController.java
@@ -0,0 +1,55 @@
+package com.haoyu.knowlagebase.controller;
+
+import com.haoyu.knowlagebase.req.DocQueryReq;
+import com.haoyu.knowlagebase.req.DocSaveReq;
+import com.haoyu.knowlagebase.resp.DocQueryResp;
+import com.haoyu.knowlagebase.resp.CommonResp;
+import com.haoyu.knowlagebase.resp.PageResp;
+import com.haoyu.knowlagebase.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+/*
+ * @author haoyu
+ * @date 2021/7/14 23:39
+ */
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all(){
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req){
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req){
+        CommonResp resp = new CommonResp<>();
+         docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+
+}
---
+++ src/main/java/com/haoyu/knowlagebase/req/DocQueryReq.java
@@ -0,0 +1,8 @@
+package com.haoyu.knowlagebase.req;
+
+public class DocQueryReq extends PageReq{
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
