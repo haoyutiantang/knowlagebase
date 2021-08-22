2edbf54994845a139f505a6c6c40819a9aff143a
haoyu <1457773856@qq.com>
2021-08-22 19:36:26 +0800

    密码加密传输和加密存储

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -75,6 +75,9 @@
   import axios from 'axios';
   import {message} from "ant-design-vue";
   import {Tool} from "@/util/tool"
+
+  declare let hexMd5: any;
+  declare let KEY: any;
   export default defineComponent({
     name:'AdminUser',
     setup(){
@@ -158,6 +161,7 @@
       const modalLoading = ref(false);
       const handleModalOk = () => {
         modalLoading.value = true;
+        user.value.password = hexMd5(user.value.password + KEY);
         axios.post("/user/save",user.value).then((response)=>{
           modalLoading.value = false;//后端有返回时就把loading效果去掉
           const data = response.data;//data = commonResp
