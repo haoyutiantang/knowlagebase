916efe3d48f34a29d53a34374e9507a946158f35
haoyu <1457773856@qq.com>
2021-08-24 02:05:35 +0800

    增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,6 +21,16 @@
             <a-menu-item key="/about">
                 <router-link to="/about">关于我们</router-link>
             </a-menu-item>
+            <a-popconfirm
+                    title="确认退出登录?"
+                    ok-text="是"
+                    cancel-text="否"
+                    @confirm="logout()"
+            >
+                <a class="login-menu" v-show="user.id">
+                <span>退出登录</span>
+                </a>
+            </a-popconfirm>
             <a class="login-menu" v-show="user.id">
                 <span>您好：{{user.name}}</span>
             </a>
@@ -83,7 +93,21 @@
                         loginModalVisible.value = false;
                         message.success("登录成功！");
 
-                        store.commit("setUser", user.value);
+                        store.commit("setUser", data.content);
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+            // 退出登录
+            const logout = () => {
+                console.log("退出登录开始");
+                axios.get('/user/logout/' + user.value.token).then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        message.success("退出登录成功！");
+                        store.commit("setUser", {});//避免空指针异常
                     } else {
                         message.error(data.message);
                     }
@@ -96,7 +120,8 @@
                 showLoginModal,
                 loginUser,
                 login,
-                user
+                user,
+                logout
             }
         }
     });
@@ -106,5 +131,6 @@
     .login-menu {
         float: right;
         color: white;
+        paddint-left: 10px;
     }
 </style>
