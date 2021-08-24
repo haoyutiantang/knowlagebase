e8d050a1fd77c8a5fc3b12d958bea6052b7bce3c
haoyu <1457773856@qq.com>
2021-08-24 12:25:24 +0800

    前后端增加登录拦截 修复BUG：前后端存储token统一用String类型

---
+++ src/main/java/com/haoyu/knowlagebase/util/LoginUserContext.java
@@ -0,0 +1,20 @@
+package com.haoyu.knowlagebase.util;
+
+
+import com.haoyu.knowlagebase.resp.UserLoginResp;
+
+import java.io.Serializable;
+
+public class LoginUserContext implements Serializable {
+
+    private static ThreadLocal<UserLoginResp> user = new ThreadLocal<>();
+
+    public static UserLoginResp getUser() {
+        return user.get();
+    }
+
+    public static void setUser(UserLoginResp user) {
+        LoginUserContext.user.set(user);
+    }
+
+}
