8143f3035298a64d7fef05618cf0a16ad3d8a3f3
haoyu <1457773856@qq.com>
2021-08-25 22:24:50 +0800

    将业务线程日志流水号传递到异步线程，方便生产运维查问题

--- src/main/java/com/haoyu/knowlagebase/service/DocService.java
+++ src/main/java/com/haoyu/knowlagebase/service/DocService.java
@@ -21,6 +21,7 @@ import com.haoyu.knowlagebase.util.SnowFlake;
 import com.haoyu.knowlagebase.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -156,7 +157,8 @@ public class DocService {
         }
         // 推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
-        wsService.sendInfo("【" + docDb.getName() + "】被点赞！");
+        String logId = MDC.get("LOG_ID");
+        wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
     }
 
     public void updateEbookInfo(){
