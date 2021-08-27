2208ea4ddef228f74ee0595c7ccf356591858d30
haoyu <1457773856@qq.com>
2021-08-27 16:29:07 +0800

    修改logo展示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
     <a-layout-header class="header">
-        <div class="logo"></div>
+        <div class="logo">haoyu知识库</div>
         <a-menu
                 theme="dark"
                 mode="horizontal"
@@ -128,9 +128,18 @@
 </script>
 
 <style>
+    .logo {
+        width: 120px;
+        height: 31px;
+        /*background: rgba(255, 255, 255, 0.2);*/
+        /*margin: 16px 28px 16px 0;*/
+        float: left;
+        color: white;
+        font-size: 18px;
+    }
     .login-menu {
         float: right;
         color: white;
-        paddint-left: 10px;
+        padding-left: 10px;
     }
 </style>
