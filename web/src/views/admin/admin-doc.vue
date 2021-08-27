862b04250d2c2dbda6aabb413f680e6956e5ce51
haoyu <1457773856@qq.com>
2021-08-27 16:46:53 +0800

    BUG修复，如果一个电子书初始没有文档，则父文档选择框无法选中"无"

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -178,7 +178,7 @@
             console.log("树形结构：",level1);
 
             // 父文档下拉框初始化，相当于点击新增
-            treeSelectData.value = Tool.copy(level1.value) || [];
+            treeSelectData.value = Tool.copy(level1.value) || [];//空数组这个小技巧为了避免空指针异常
             // 为选择树添加一个"无"
             treeSelectData.value.unshift({id: 0, name: '无'});
           }else{
